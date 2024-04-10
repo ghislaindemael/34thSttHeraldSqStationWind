33f885dec2c82f157ded4cab6900b95c82f8ab4a
Ghislain Demaël <demaelghislain@gmail.com>
2024-04-10 11:05:45 +0200

    Setup server configuration & loading of index.html

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-import("./downloadExtractData.js")
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+//import("./public/js/downloadExtractData.js")
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
