f7049b739b1966bfc46be62e6b002242dc1376ac
Ghislain Demaël <demaelghislain@gmail.com>
2024-05-01 18:53:01 +0200

    Set ws back to decrease with distance

--- public/js/Link.js
+++ public/js/Link.js
@@ -46,8 +46,8 @@ export class Link {
         if(isNaN(this.windDirection)){
             console.log("Input winddir is nan");
         }
-        //this.windStrength = Math.round((1 - (this.roomDistance / 10000)) * startWS);
-        this.windStrength = startWS;
+        this.windStrength = Math.round((1 - (this.roomDistance / 10000)) * startWS);
+        //this.windStrength = startWS;
 
 
     }
